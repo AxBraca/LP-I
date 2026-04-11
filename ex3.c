620b1ffbd7df2944e0193eeb1a716ab90098d3d9
braca <ax.braca@gmail.com>
2026-04-10 21:02:31 -0300

    ex3 terminado

--- ex3.c
+++ ex3.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 typedef struct Data{
@@ -20,63 +21,89 @@ typedef struct Reserva{
     char status; //P ou N
 } Reserva;
 
+typedef struct Resumo{
+    char destino[31];
+    int qnt;
+} Resumo;
+
 int main(){
     Reserva reserva[10];
-    int menu,qntReservas = 0, i=0, anoFiltro, mesFiltro;
+    int menu,qntReservas = 0, i=0, anoFiltro, mesFiltro, totalUnicos = 0, encontrado = 0;
+    Resumo resumo[10];
 
-
-printf("1 - Cadastrar Reservas\n");
+do {
+printf("\n1 - Cadastrar Reservas\n");
 printf("2 - Exibir Reservas\n");
-printf("3 - Exibir apenas reservas pagas\n"); 
+printf("3 - Exibir apenas reservas pagas\n");
 printf("4 - Filtro por data\n");
-printf("5 - Resumo por destino\n");   
+printf("5 - Resumo por destino\n");
+printf("0 - Sair\n");
+scanf("%d", &menu);
 switch (menu)
 {
     case 1:
-        // Cadastro    
+        // Cadastro
         printf("Qual a quantidade de Reservas: ");
         scanf("%d", &qntReservas);
 
-        for (int i=0; i < qntReservas; i++) {
+        for (int i = 0; i < qntReservas; i++) {
             printf("Codigo: ");
             scanf("%d", &reserva[i].codigo);
+            while (getchar() != '\n'); 
 
             printf("Digite o Nome do Cliente: ");
-            gets(reserva[i].cliente);
+            fgets(reserva[i].cliente, 51, stdin);
+            // Remover o \n manualmente ;C
+            for (int j = 0; j < 51; j++) {
+                if (reserva[i].cliente[j] == '\n') {
+                    reserva[i].cliente[j] = '\0';
+                    break;
+                }
+            }
 
             printf("Digite o Destino do Cliente: ");
-            gets(reserva[i].destino);
+            fgets(reserva[i].destino, 31, stdin);
+            //limpa
+            for (int j = 0; j < 31; j++) {
+                if (reserva[i].destino[j] == '\n') {
+                    reserva[i].destino[j] = '\0';
+                    break;
+                }
+            }
 
-            printf("Digite a Data da Viagem: ");
-            scanf("%hhu", &reserva[i].dataViagem.dia);
-            scanf("%hhu", &reserva[i].dataViagem.mes);
-            scanf("%hu", &reserva[i].dataViagem.ano);
+            printf("Digite a Data da Viagem (DD MM AAAA): ");
+            scanf(" %hhu %hhu %hu", 
+                &reserva[i].dataViagem.dia, 
+                &reserva[i].dataViagem.mes, 
+                &reserva[i].dataViagem.ano);
+            while (getchar() != '\n'); 
 
-            printf("Insira o Status da Reserva: ");
-            scanf("%c", &reserva[i].status);
+            printf("Insira o Status da Reserva(P ou N): ");
+            scanf(" %c", &reserva[i].status);
+            while (getchar() != '\n'); 
         }
     break;
 
     case 2:
         // Exibição de Reservas
-        printf("COD\t| Cliente\t| Destino\t| Data\t| Status \n");
+        printf("%-6s | %-20s | %-15s | %-10s | %s\n", "COD", "CLIENTE", "DESTINO", "DATA", "STATUS");
         for (int i=0; i < qntReservas; i++) {
-            printf("%d\t | %s\t| %s\t| %hhu/%hhu/%u\t| %s\t\n", 
-            reserva[i].codigo,
-            reserva[i].cliente,
-            reserva[i].destino,
-            reserva[i].dataViagem.dia,
-            reserva[i].dataViagem.mes,
+            printf("%-6d | %-20.20s | %-15.15s | %02hhu/%02hhu/%-5u | %c\n", //-1.1s para regularizar os espaços
+            reserva[i].codigo, 
+            reserva[i].cliente, 
+            reserva[i].destino, 
+            reserva[i].dataViagem.dia, 
+            reserva[i].dataViagem.mes, 
             reserva[i].dataViagem.ano,
             reserva[i].status);
-        }
+        } 
     break;
 
     case 3:
         // Filtro: Reservas Pagas
         for (int i=0; i < qntReservas; i++) {
             if (reserva[i].status == 'P') {
-                printf("%d\t | %s\t| %s\t| %hhu/%hhu/%u\tn", 
+                printf("%-6d | %-20.20s | %-15.15s | %02hhu/%02hhu/%-5u\n",
                 reserva[i].codigo,
                 reserva[i].cliente,
                 reserva[i].destino,
@@ -89,31 +116,49 @@ switch (menu)
 
     case 4:
     // Filtro: Por Data
-    printf("Digite o mes da viagem: ");
-    scanf("%hhu", &mesFiltro);
+        printf("Digite a Data da Viagem (MM AAAA): ");
+            scanf(" %i %i",  
+                &mesFiltro, 
+                &anoFiltro);
 
-    printf("Digite o ano da viagem: ");
-    scanf("%u", &anoFiltro);
-
-    for (int i=0; i < qntReservas; i++) {
-        if (reserva[i].dataViagem.mes == mesFiltro && reserva[i].dataViagem.ano == anoFiltro){
-            printf("%d\t | %s\t| %s\t| %s\t\n", 
-            reserva[i].codigo,
-            reserva[i].cliente,
-            reserva[i].destino,
-            reserva[i].status);
+        for (int i=0; i < qntReservas; i++) {
+            if (reserva[i].dataViagem.mes == mesFiltro && reserva[i].dataViagem.ano == anoFiltro){
+                printf("%-6d | %-20.20s | %-15.15s | %c\n",
+                reserva[i].codigo,
+                reserva[i].cliente,
+                reserva[i].destino,
+                reserva[i].status);
+            }
         }
-    }
-    break;
+        break;
 
     case 5:
-    // Filtro: Por Destino
-    for (int i=0; i < qntReservas; i++) 
-    {
-        
-    }
-    break;
+        // Filtro: Por Destino
+        for (int i = 0; i < qntReservas; i++) {
+            encontrado = 0;
 
- }
+            for (int j = 0; j < totalUnicos; j++) {
+                if (strcmp(reserva[i].destino, resumo[j].destino) == 0) {
+                    resumo[j].qnt++;
+                    encontrado = 1; 
+                    break;
+                }
+            }
+
+            if (!encontrado) {
+                strcpy(resumo[totalUnicos].destino, reserva[i].destino);
+                resumo[totalUnicos].qnt = 1;
+                totalUnicos++; 
+            }
+        }
+
+        for (int i=0; i < totalUnicos; i++) {
+            printf("%s\t| %d\n",
+            resumo[i].destino,
+            resumo[i].qnt);
+        }
+    break;
+   }
+  }while (menu !=0);
 }
 
